9193d82b6446fe1fe153d3fa8680e5200a0a1da8
josephlarrivy <joseph.larrivy@gmail.com>
2023-02-11 17:35:29 -0600

    I'm not giving up

--- count-zeroes.js
+++ count-zeroes.js
@@ -13,74 +13,129 @@
 
 // countZeroes([1,0,0,0])
 
-function countZeroes(array) {
-  // let arr = array
-  // let leftIdx = 0;
-  // let leftVal = 1;
-  // let rightIdx = arr.length - 1;
-  // let middleIdx;
+// function countZeroes(array) {
+//   let arr = array
+//   let leftIdx = 0;
+//   let leftVal = 1;
+//   let rightIdx = arr.length - 1;
+//   let middleIdx;
 
-  // while (leftVal !== 0) {
-  //   // find the middle value
-  //   middleIdx = Math.floor((leftIdx + rightIdx) / 2);
-  //   let middleVal = arr[middleIdx];
+//   while (leftVal !== 0) {
+//     // find the middle value
+//     middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+//     let middleVal = arr[middleIdx];
 
-  //   if (leftVal !== 0) {
-  //     console.log('middleVal is not zero, move it right')
-  //     leftIdx = middleIdx - 1;
-  //     leftVal = arr[leftIdx]
-  //     arr = arr.slice(leftIdx, rightIdx + 1)
-  //   } else if (middleVal == 0) {
-  //     // console.log('middleVal is zero, look at the left half')
-  //     // rightIdx = middleIdx;
-  //     // arr = arr.slice(leftIdx, rightIdx)
-  //   }
-  //   // left and right pointers crossed, val isn't in arr
-  //   console.log(arr)
-  //   console.log(`middleIdx: ${middleIdx}`)
-  //   console.log(`LOOP`)
+//     if (leftVal !== 0) {
+//       console.log('middleVal is not zero, move it right')
+//       leftIdx = middleIdx - 1;
+//       leftVal = arr[leftIdx]
+//       arr = arr.slice(leftIdx, rightIdx + 1)
+//     } else if (middleVal == 0) {
+//       // console.log('middleVal is zero, look at the left half')
+//       // rightIdx = middleIdx;
+//       // arr = arr.slice(leftIdx, rightIdx)
+//     }
+//     // left and right pointers crossed, val isn't in arr
+//     console.log(arr)
+//     console.log(`middleIdx: ${middleIdx}`)
+//     console.log(`LOOP`)
     
-  // }
-  // numZeroes = (array.length - 1) - (middleIdx)
-  // console.log(numZeroes)
-  // return numZeroes
+//   }
+//   numZeroes = (array.length - 1) - (middleIdx)
+//   console.log(numZeroes)
+//   return numZeroes
+// }
 
 
+// function countZeroes(array) {
 
-  let arr = array;
+//   let arr = array;
 
-  let leftIdx = arr[0]
-  let rightIdx = arr[arr.length - 1]
-  let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+//   let leftIdx = arr[0]
+//   let rightIdx = arr[arr.length - 1]
+//   let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+//   let firstZeroIdx
 
+//   const findFirstZero = (arr) => {
+//     if (arr[middleIdx] == 0 && arr[middleIdx -1] == 1) {
+//       firstZeroIdx = middleIdx;
 
+//       console.log(middleIdx)
+//       // findFirstZero(arr.slice(leftIdx, rightIdx))
+//       return
 
+//     } else if (arr[middleIdx] == 1) {
+//       leftIdx = middleIdx - 1;
 
+//       console.log(arr)
+//       findFirstZero(arr.slice(leftIdx, rightIdx))
 
-  if (arr[0] == 0) {
-    console.log(arr.length)
-    return arr.length
-  }
+//     } else if (arr[middleIdx] == 0) {
+//       rightIdx = middleIdx +1
 
-  while (leftIdx !== 0) {
-    if (middleIdx == 0) {
-      rightIdx = middleIdx +1;
-      arr = arr.slice(leftIdx, rightIdx);
-    } else if (middleIdx == 1) {
-      leftIdx = middleIdx -1;
-      arr = arr.slice(leftIdx, rightIdx)
-    }
-    console.log(arr)
-    console.log(leftIdx, rightIdx)
-    console.log('############')
-  }
-
+//       console.log(arr)
+//       findFirstZero(arr.slice(leftIdx, rightIdx))
+      
+//     }
+    
+//   }
   
+//  findFirstZero(array)
+
+
+// }
+// console.log(countZeroes([1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0]))
+// countZeroes([0, 0])
+
+// module.exports = countZeroes
+
+
+// function countZeroes(array) {
+
+
+//   const findFirst = (arr, leftIdx = 0, rightIdx = array.length - 1) => {
+//     if (arr[leftIdx] == 0) {
+//       // console.log(leftIdx)
+//       return leftIdx;
+//     } else {
+//       leftIdx = leftIdx + 1
+//       return findFirst(array, leftIdx, rightIdx)
+//     }
+//   }
   
+//   return (array.length - findFirst(array))
   
 
+// }
+
+// console.log(countZeroes([1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0]))
+// // console.log(countZeroes([1, 1, 0, 0]))
+
+// module.exports = countZeroes
+
+
+
+
+
+function countZeroes(arr) {
+  // add whatever parameters you deem necessary - good luck!
+  let firstZero = findFirst(arr)
+  if (firstZero === -1) return 0;
+
+  return arr.length - firstZero
 }
-countZeroes([1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0])
-countZeroes([0, 0])
+
+function findFirst(arr, low = 0, high = arr.length - 1) {
+  if (high >= low) {
+    let mid = low + Math.floor((high - low) / 2)
+    if ((mid === 0 || arr[mid - 1] === 1) && arr[mid] === 0) {
+      return mid;
+    } else if (arr[mid] === 1) {
+      return findFirst(arr, mid + 1, high)
+    }
+    return findFirst(arr, low, mid - 1)
+  }
+  return -1;
+}
 
 module.exports = countZeroes
